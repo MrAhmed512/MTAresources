5d8969f6d0004ad5b8cf3d0010d9b79bf8ff8059
MrAhmed512 <43019381+MrAhmed512@users.noreply.github.com>
2019-08-26 23:22:31 +0300

    Create convertToDGS.lua

---
+++ convertToDGS.lua
@@ -0,0 +1,45 @@
+--Programmed by MrAhmed
+function guiCreateWindow(...)
+    local window = exports.dgs:dgsCreateWindow(...)
+    return window
+end
+function guiCreateButton(...)
+    local button = exports.dgs:dgsCreateButton(...)
+    return button
+end
+function guiCreateGridlist(...)
+    local grid = exports.dgs:dgsCreateGridList(...)
+    return grid
+end
+function guiCreateLabel(...)
+    local label = exports.dgs:dgsCreateLabel(...)
+    return label
+end
+function guiCreateProgressBar(...)
+    local bar = exports.dgs:dgsCreateProgressBar(...)
+    return bar
+end
+function guiCreateRadioButton(...)
+    local radio = exports.dgs:dgsCreateRadioButton(...)
+    return radio
+end
+function guiCreateTabPanel(...)
+    local tabpanel = exports.dgs:dgsCreateTabPanel(...)
+    return tabpanel
+end
+function guiCreateStaticImage(...)
+    local image = exports.dgs:dgsCreateImage(...)
+    return image
+end
+function guiCreateEdit(...)
+    local edit = exports.dgs:dgsCreateEdit(...)
+    return edit
+end
+function guiCreateComboBox(...)
+    local combo = exports.dgs:dgsCreateComboBox(...)
+    return combo
+end
+function guiCreateCheckBox(...)
+    local check = exports.dgs:dgsCreateCheckBox(...)
+    return check
+end
